96303172774d831602e62c1cd174ff31864639aa
NicoleKai <nicolekohm102@gmail.com>
2023-10-24 19:54:19 -0600

    i am suffering

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,32 @@ use bevy_math::Vec3;
 use derive_new::new;
 
 #[derive(Debug, Clone)]
+enum LimbComponent {
+    joint(Joint),
+    segment(Segment),
+}
+
+#[derive(Debug, Clone)]
+pub struct Segment {
+    segment_name: Arc<str>,
+    position: Vec3,
+    length: f32,
+    joint: Joint,
+}
+
+#[derive(Debug, Clone)]
 pub struct Joint {
-    joint_name: Arc<str>,
     position: Vec3,
 }
 
-impl Joint {
-    pub fn new(joint_name: impl Into<Arc<str>>, position: Vec3) -> Self {
-        Self {
-            joint_name: joint_name.into(),
-            position,
-        }
-    }
-}
+// impl Segment {
+//     pub fn new(segment_name: impl Into<Arc<str>>, position: Vec3) -> Self {
+//         Self {
+//             segment_name: segment_name.into(),
+//             joint: Joint { position },
+//         }
+//     }
+// }
 
 #[inline]
 fn compute_iterations(bounce_iterations: usize, length: usize) -> usize {
@@ -33,7 +46,7 @@ enum BounceState {
 pub struct Limb {
     bounce_iterations: usize,
     iterations: usize,
-    joints: Vec<Joint>,
+    components: Vec<LimbComponent>,
 }
 
 enum MathState {
@@ -43,18 +56,19 @@ enum MathState {
 }
 
 impl Limb {
-    pub fn new(bounce_iterations: usize, joints: Vec<Joint>) -> Self {
+    pub fn new(bounce_iterations: usize, joints: Vec<LimbComponent>) -> Self {
         let len = joints.len();
         Self {
             // TODO: bounce iterations can be changed dynamically
             bounce_iterations: 1,
             iterations: compute_iterations(bounce_iterations, len),
-            joints,
+            components: joints,
         }
     }
     pub fn satisfice(&mut self, target_end_effector: Vec3) {
         // TODO: handle niche cases, e.g., joint count <= 2
-        let v_len = self.joints.len();
+        let v_len = self.components.len();
+        let iterations = (v_len - 1) * 2 + 1;
         dbg!(&v_len);
         if v_len <= 0 {
             return;
@@ -63,29 +77,33 @@ impl Limb {
         let mut math_state = MathState::Initialization;
         let mut bounce_state = BounceState::Forward;
         for _bounce_counter in 0..self.bounce_iterations {
-            (*self.joints.last_mut().expect("crabs attacking")).position = target_end_effector;
-            // dbg!("END EFFECTOR", &self.joints.);
+            
+            // (*self.components.last_mut().expect("crabs attacking")).position = target_end_effector;
+            // // dbg!("END EFFECTOR", &self.joints.);
 
-            for _counter in 0..((v_len - 1) * 2 + 1) {
-                if index >= v_len {
-                    bounce_state = BounceState::Backward;
-                    // Will break for lengths less than 2 obvs
-                    index = v_len - 2;
-                }
-                if index <= 0 {
-                    bounce_state = BounceState::Forward;
-                }
-                dbg!(&self.joints[index]);
-                match bounce_state {
-                    BounceState::Forward => index += 1,
-                    BounceState::Backward => index -= 1,
-                }
+            // for _counter in 0..iterations {
+            //     if index >= v_len {
+            //         bounce_state = BounceState::Backward;
+            //         // Will break for lengths less than 2 obvs
+            //         index = v_len - 2;
+            //     }
+            //     if index <= 0 {
+            //         bounce_state = BounceState::Forward;
+            //     }
+            //     let mut ptr_current = self.components.get(index).unwrap();
+            //     let mut ptr_next = self.components.get(index + 1).unwrap();
+            //     let diff = ptr_next.position - ptr_current.position;
+            //     let pos = (ptr_next.position - ptr_current.position).normalize();
+            //     match bounce_state {
+            //         BounceState::Forward => index += 1,
+            //         BounceState::Backward => index -= 1,
+            //     }
             }
         }
     }
 }
 
-#[cfg(test)]
+// #[cfg(test)]
 mod tests {
     use super::*;
 
