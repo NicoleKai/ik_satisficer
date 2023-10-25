ff62e15735483108e269d6a83c8543f6733edbdb
NicoleKai <nicolekohm102@gmail.com>
2023-10-24 20:16:57 -0600

    pls

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,14 @@ use derive_new::new;
 
 #[derive(Debug, Clone)]
 enum LimbComponent {
-    joint(Joint),
-    segment(Segment),
+    Joint(Joint),
+    Segment(Segment),
+    Terminus(Terminus),
+}
+
+pub enum Terminus {
+    EndEffector,
+    Ground,
 }
 
 #[derive(Debug, Clone)]
@@ -66,40 +72,47 @@ impl Limb {
         }
     }
     pub fn satisfice(&mut self, target_end_effector: Vec3) {
-        // TODO: handle niche cases, e.g., joint count <= 2
-        let v_len = self.components.len();
-        let iterations = (v_len - 1) * 2 + 1;
-        dbg!(&v_len);
-        if v_len <= 0 {
-            return;
-        }
-        let mut index = v_len - 1;
-        let mut math_state = MathState::Initialization;
-        let mut bounce_state = BounceState::Forward;
-        for _bounce_counter in 0..self.bounce_iterations {
-            
-            // (*self.components.last_mut().expect("crabs attacking")).position = target_end_effector;
-            // // dbg!("END EFFECTOR", &self.joints.);
-
-            // for _counter in 0..iterations {
-            //     if index >= v_len {
-            //         bounce_state = BounceState::Backward;
-            //         // Will break for lengths less than 2 obvs
-            //         index = v_len - 2;
-            //     }
-            //     if index <= 0 {
-            //         bounce_state = BounceState::Forward;
-            //     }
-            //     let mut ptr_current = self.components.get(index).unwrap();
-            //     let mut ptr_next = self.components.get(index + 1).unwrap();
-            //     let diff = ptr_next.position - ptr_current.position;
-            //     let pos = (ptr_next.position - ptr_current.position).normalize();
-            //     match bounce_state {
-            //         BounceState::Forward => index += 1,
-            //         BounceState::Backward => index -= 1,
-            //     }
+        for something in self.components {
+            match something {
+                LimbComponent::Joint(_) => todo!(),
+                LimbComponent::Segment(_) => todo!(),
+                LimbComponent::Terminus(_) => todo!(),
             }
         }
+        //     // TODO: handle niche cases, e.g., joint count <= 2
+        //     let v_len = self.components.len();
+        //     let iterations = (v_len - 1) * 2 + 1;
+        //     dbg!(&v_len);
+        //     if v_len <= 0 {
+        //         return;
+        //     }
+        //     let mut index = v_len - 1;
+        //     let mut math_state = MathState::Initialization;
+        //     let mut bounce_state = BounceState::Forward;
+        //     for _bounce_counter in 0..self.bounce_iterations {
+
+        //         // (*self.components.last_mut().expect("crabs attacking")).position = target_end_effector;
+        //         // // dbg!("END EFFECTOR", &self.joints.);
+
+        //         // for _counter in 0..iterations {
+        //         //     if index >= v_len {
+        //         //         bounce_state = BounceState::Backward;
+        //         //         // Will break for lengths less than 2 obvs
+        //         //         index = v_len - 2;
+        //         //     }
+        //         //     if index <= 0 {
+        //         //         bounce_state = BounceState::Forward;
+        //         //     }
+        //         //     let mut ptr_current = self.components.get(index).unwrap();
+        //         //     let mut ptr_next = self.components.get(index + 1).unwrap();
+        //         //     let diff = ptr_next.position - ptr_current.position;
+        //         //     let pos = (ptr_next.position - ptr_current.position).normalize();
+        //         //     match bounce_state {
+        //         //         BounceState::Forward => index += 1,
+        //         //         BounceState::Backward => index -= 1,
+        //             // }
+        //         }
+        //     }
     }
 }
 
@@ -111,14 +124,16 @@ mod tests {
     fn it_works() {
         // let result = add(2, 2);
         // assert_eq!(result, 4);
-        let mut joints = Vec::new();
-        joints.push(Joint::new("ground", Vec3::new(0., 0., 0.)));
-        joints.push(Joint::new("first joint", Vec3::new(1., 2., 3.)));
-        joints.push(Joint::new("second joint", Vec3::new(4., 5., 6.)));
-        joints.push(Joint::new("end effector", Vec3::new(7., 8., 9.)));
-        let mut limb = Limb::new(1, joints);
+        let mut limb_vec = Vec::new();
 
-        limb.satisfice(Vec3::ZERO);
+        limb_vec.push(Terminus::Ground);
+        // joints.push(Joint::new("ground", Vec3::new(0., 0., 0.)));
+        // joints.push(Joint::new("first joint", Vec3::new(1., 2., 3.)));
+        // joints.push(Joint::new("second joint", Vec3::new(4., 5., 6.)));
+        // joints.push(Joint::new("end effector", Vec3::new(7., 8., 9.)));
+        // let mut limb = Limb::new(1, joints);
+
+        // limb.satisfice(Vec3::ZERO);
 
         assert!(false);
     }
